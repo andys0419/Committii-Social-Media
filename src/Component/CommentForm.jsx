92ce07c2faef1eac95182b84cc652a28304fc858
ashi0419 <andyshi@buffalo.edu>
2021-04-01 01:54:10 -0400

    #88 Comment Form UI

--- src/Component/CommentForm.jsx
+++ src/Component/CommentForm.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import "../App.css";
+import { Link } from 'react-router-dom';
+import committiilogo from "../assets/logo.svg";
 import PostingList from "./PostingList.jsx";
+import "./CommentForm.css"
 
 export default class CommentForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       post_text: "",
-      postmessage: ""
+      postmessage: "",
+      comment_holder: "You are commenting as: " + sessionStorage.getItem("username"),
+      errorMessage: ""
     };
     this.postListing = React.createRef();
   }
@@ -16,10 +20,18 @@ export default class CommentForm extends React.Component {
     //keep the form from actually submitting
     event.preventDefault();
 
+    const postMsg = this.state.postmessage;
+
+    if (postMsg == '') {
+      this.setState({
+        errorMessage: "Please enter a message."
+      })
+    }
+
     //make the api call to the authentication page
 
-    fetch(process.env.REACT_APP_API_PATH+"/posts", {
-      method: "post",
+    fetch("https://webdev.cse.buffalo.edu/hci/elmas/api/api/"+"/posts", {
+      method: "POST",
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer '+sessionStorage.getItem("token")
@@ -51,27 +63,53 @@ export default class CommentForm extends React.Component {
     });
   };
 
+  updatePostMessage = (e) => {
+    this.setState({
+      postmessage: e.currentTarget.value
+    })
+  }
+
   render() {
     return (
-      <div>
-        <form onSubmit={this.submitHandler}>
-          <label>
-            Add A Comment to Post {this.props.parent}
-            <br />
-            <textarea rows="10" cols="70" onChange={this.myChangeHandler} />
-          </label>
-          <br />
 
-          <input type="submit" value="submit" />
-          <br />
-          {this.state.postmessage}
-        </form>
-        <PostingList
-          ref={this.postListing}
-          parentid={this.props.parent}
-          type="commentlist"
-        />
-      </div>
+      <div id="logo">
+        <Link to="/pollpage"><img id="committi_logo" src={committiilogo}></img></Link>
+
+        <div id="current_user">
+        Add A Comment to this Poll.{this.props.parent}
+            <br/>
+            <br/>
+
+            <div id="comment_box">
+                 <textarea onChange={this.updatePostMessage} value={this.state.postmessage} rows="8" cols="43" placeholder={this.state.comment_holder} onChange={this.myChangeHandler}/>
+            </div>
+            <br/>
+
+            <div id="line">
+                <div id="comments">
+                    <text>Current Comments</text>
+                    <br/>
+                    <text1>(No Comments currently exist!)</text1>
+                </div>
+            </div>
+            
+            
+            <form onSubmit={this.submitHandler}>
+            <input type="submit" value="Comment"/>
+            {/* {this.state.postmessage} */}
+            <br/>
+            {this.state.errorMessage !== "" ? this.state.errorMessage : <div/>}
+            {this.state.errorMessage == ""}
+            <br />
+            </form>
+
+            {/* <PostingList
+                 ref={this.postListing}
+                 parentid={this.props.parent}
+                 type="commentlist"
+            /> */}
+        </div>
+    </div>
     );
   }
 }
