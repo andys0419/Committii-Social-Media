abb8d680db2de20e3107cae1c47f5002764e2672
chrispalla129 <chrispalla129@gmail.com>
2021-04-08 18:56:14 -0400

    user-story#101 Finished Implementation

--- src/Component/feed.jsx
+++ src/Component/feed.jsx
@@ -6,6 +6,7 @@ import committiilogo from "../assets/logo.svg";
 import backarrow from "../assets/back_arrow.svg";
 import prof_pic from "../assets/profile-picture-holder.png";
 import hearticon from "../assets/heart-icon.svg";
+import Post from "./Post.jsx";
 
 import {
     Redirect
@@ -19,9 +20,11 @@ export default class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      posts: [],
       username: "",
       password: "",
       confirm: "",
+      listType: props.listType,
       alanmessage: "",
       errormessage: "",
       sessiontoken: "",
@@ -29,7 +32,117 @@ export default class Register extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.loadPosts();
+    fetch(process.env.REACT_APP_API_PATH+"/users/"+sessionStorage.getItem("user"), {
+      method: "get",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+sessionStorage.getItem("token")
+      }
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result) {
+            console.log(result);
+
+            this.setState({
+              // IMPORTANT!  You need to guard against any of these values being null.  If they are, it will
+              // try and make the form component uncontrolled, which plays havoc with react
+              username: result.username || "",
+              email: result.email || "",
+              //lastname: result.lastName || ""
+
+            });
+          }
+        },
+        error => {
+          alert("error!");
+        }
+      );
+  }
+  
+  loadPosts() {
+    let url = process.env.REACT_APP_API_PATH+"/posts?ParentID="+sessionStorage.getItem("user");
+    // /posts/1
+    if (this.props && this.props.parentid){
+      url += this.props.parentid;
+    }
+    fetch(url, {
+      method: "get",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+sessionStorage.getItem("token")
+      },
+
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result) {
+            this.setState({
+              isLoaded: true,
+              posts: result[0]
+            });
+            console.log("Got Posts");
+          }
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+          console.log("ERROR loading Posts")
+        }
+      );
+  }
+
+  createPost(post) {
+    let content = post.content.split("vs.");
+    let likes = 0;
+    let comments = post.commentCount;
+    let id = "pollpage/" + post.id.toString();
+
+    CanvasJS.addColorSet("gray_color",
+    ["#acacac"]);
+    const options = {
+        responsive: true,
+        maintainAspectRation: false,
+        axisY: {interval: 1, labelFontSize: 15},
+        axisX: {labelFontSize: 16},
+        width: 600,
+        height: 245,
+        colorSet: "gray_color",
+        title: {
+        text: ""
+        
+      },
+      data: [{				
+                type: "column",
+                dataPoints: [
+                    { label: content[0].trim(), y: Math.floor(Math.random() * 10) },
+                    { label: content[1].trim(), y: Math.floor(Math.random() * 10)  },
+                ]
+       }]
+   }
+   return (
+    <div class = "feedPosts">
+      <div class = "post">
+        <p class="likeButton">{likes} Likes</p>
+        <Link to="/pollpage"><button class="commentButton">{comments} Comments</button></Link>
+        <Link to={id}><button class="postButton">View Post</button></Link>
+        <div class="chart">
+          <CanvasJSChart options = {options}></CanvasJSChart>
+        </div>
+      </div>
+    </div>
+   )
+
+  }
+
   render() {
+    const {posts} = this.state;
     CanvasJS.addColorSet("gray_color",
     ["#acacac"]);
     const options = {
@@ -53,10 +166,12 @@ export default class Register extends React.Component {
        }]
    }
     return (
+      
       <div class = "feed">
-        <Link to="/">
+        <Link to="/feed">
           <img id="committii-logo" src={committiilogo}></img>
         </Link>
+
         <div class="feedOptions">
           <div class="vLeft">
 
@@ -64,22 +179,14 @@ export default class Register extends React.Component {
           </div>
           <div class="vRight">
             <button class="feedMessages">Messages</button>
-        </div>
+          </div>
           
           <Link to="/profile"><button class="feedProfile">Profile</button></Link>
-          
-          <div class = "feedPosts">
-            <div class = "post">
-              <p class="likeButton">Likes: 20</p>
-              <Link to="/pollpage"><button class="commentButton">8 Comments</button></Link>
-              <div class="chart">
-                <CanvasJSChart options = {options}></CanvasJSChart>
-              </div>
-            </div>
-            <div class = "post">
-            </div>
-          </div>
         </div>
+
+        {this.state.posts.map(post => this.createPost(post))}
+
+        
       </div>
     );
   }
