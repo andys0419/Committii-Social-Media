c7469da93850655c38f3933986213c17f8bbc038
jefitzpa <47531013+jefitzpa@users.noreply.github.com>
2021-04-15 12:39:34 -0400

    Img Fix

--- src/Component/ProfileSettings.jsx
+++ src/Component/ProfileSettings.jsx
@@ -182,8 +182,8 @@ export default class ProfileSettings extends React.Component {
        if (result.role == ""){
          document.getElementById("profilepic").src = prof_pic
        }else{
-       var server = process.env.REACT_APP_API_PATH.slice(0, -4) + "/";
-       
+       var server = "https://webdev.cse.buffalo.edu/hci/elmas"
+
        document.getElementById("profilepic").src = server + result.role
      }
      })
