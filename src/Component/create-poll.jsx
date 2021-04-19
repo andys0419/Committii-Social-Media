4805e13c87269530a6ffa5b2ba90a6bc3bab8947
jefitzpa <47531013+jefitzpa@users.noreply.github.com>
2021-04-19 15:51:18 -0400

    Images can be displayed on posts
    Need to make it look good

--- src/Component/create-poll.jsx
+++ src/Component/create-poll.jsx
@@ -26,17 +26,14 @@ export default class PostForm extends React.Component {
       redir: false
     };
     this.postListing = React.createRef();
+    this.uploadImages = this.uploadImages.bind(this);
+    this.makepostImage = this.makepostImage.bind(this);
   }
 
   // the handler for submitting a new post.  This will call the API to create a new post.
   // while the test harness does not use images, if you had an image URL you would pass it
   // in the thumbnailURL field.
-  submitHandler = event => {
-
-    //keep the form from actually submitting via HTML - we want to handle it in react
-    event.preventDefault();
-
-    //make the api call to post
+  makepostImage(url1, url2){
     fetch(process.env.REACT_APP_API_PATH+"/posts", {
       method: "post",
       headers: {
@@ -48,7 +45,7 @@ export default class PostForm extends React.Component {
         content: this.state.post_text,
         parentID: this.props.parent,
         thumbnailURL: "",
-        type: this.state.poll_category
+        type: JSON.stringify({"tag": this.state.poll_category, "images": [url1, url2]})
       })
     })
       .then(res => res.json())
@@ -58,6 +55,75 @@ export default class PostForm extends React.Component {
             postmessage: result.Status,
             redir: true
           });
+          console.log(result)
+        },
+        error => {
+          alert("error!");
+        }
+      );
+  }
+  uploadImages(id1, id2){
+    var server = process.env.REACT_APP_API_PATH.slice(0, -4) + "/"
+    let url1 = ""
+    var formData = new FormData();
+    var fileInput = document.getElementById('imgUpload1');
+    formData.append("file", fileInput.files[0]);
+    fetch(process.env.REACT_APP_API_PATH + "/user-artifacts/" + String(id1) + "/upload", {
+      method: "POST",
+      body: formData,
+      headers: {
+        'Authorization': 'Bearer '+ sessionStorage.getItem("token")
+      },
+    }).then(res => res.json())
+    .then(
+      result => {
+        url1 = server + result.url
+        let url2 = ""
+        formData = new FormData();
+        fileInput = document.getElementById('imgUpload2');
+        formData.append("file", fileInput.files[0]);
+        fetch(process.env.REACT_APP_API_PATH + "/user-artifacts/" + String(id2) + "/upload", {
+          method: "POST",
+          body: formData,
+          headers: {
+            'Authorization': 'Bearer '+ sessionStorage.getItem("token")
+          },
+        }).then(res => res.json())
+        .then(
+          result => {
+            url2 = server + result.url
+            this.makepostImage(url1, url2)
+          })
+      })
+  }
+  submitHandler = event => {
+
+    //keep the form from actually submitting via HTML - we want to handle it in react
+    event.preventDefault();
+
+    if (document.getElementById("imgUpload1").value == "" && document.getElementById("imgUpload2").value == ""){
+    fetch(process.env.REACT_APP_API_PATH+"/posts", {
+      method: "post",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+sessionStorage.getItem("token")
+      },
+      body: JSON.stringify({
+        authorID: sessionStorage.getItem("user"),
+        content: this.state.post_text,
+        parentID: this.props.parent,
+        thumbnailURL: "",
+        type: JSON.stringify({"tag": this.state.poll_category, "images": []})
+      })
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            postmessage: result.Status,
+            redir: true
+          });
+          console.log(result)
           // once a post is complete, reload the feed
           //this.postListing.current.loadPosts();
         },
@@ -65,6 +131,57 @@ export default class PostForm extends React.Component {
           alert("error!");
         }
       );
+    }else{
+      if (document.getElementById("imgUpload1").value == "" || document.getElementById("imgUpload2").value == ""){
+        alert("Missing an Image")
+      }else{
+        let id1;
+        fetch(process.env.REACT_APP_API_PATH + "/user-artifacts", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+ sessionStorage.getItem("token")},
+          body: JSON.stringify({
+            "ownerID": sessionStorage.getItem("user"),
+            "type": "Image",
+            "url": "String",
+            "category": "postpic"
+          })
+        })
+        .then(res => res.json())
+        .then(
+          result => {
+            var userArtifact = -1;
+            userArtifact = result.id;
+            if (userArtifact != -1){
+              id1 = userArtifact
+            }
+            fetch(process.env.REACT_APP_API_PATH + "/user-artifacts", {
+              method: "POST",
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ sessionStorage.getItem("token")},
+              body: JSON.stringify({
+                "ownerID": sessionStorage.getItem("user"),
+                "type": "Image",
+                "url": "String",
+                "category": "postpic"
+              })
+            })
+            .then(res => res.json())
+            .then(
+              result => {
+                var userArtifact = -1;
+                userArtifact = result.id;
+                let urls = []
+                console.log(urls)
+                if (userArtifact != -1){
+                  this.uploadImages(id1, userArtifact);
+                }
+              })
+          })
+      }
+    }
   };
 
   // this method will keep the current post up to date as you type it,
@@ -108,6 +225,14 @@ export default class PostForm extends React.Component {
                 placeholder={" Category"}
                 onChange={e => this.fieldChangeHandler("poll_category", e)}
             />
+            <form action="/action_page.php" id="image1">
+              <label for="img1">Select Image:</label>
+              <input type="file" id="imgUpload1" name="img" accept="image/*"></input>
+            </form>
+            <form action="/action_page.php" id="image2">
+              <label for="img1">Select Image:</label>
+              <input type="file" id="imgUpload2" name="img" accept="image/*"></input>
+            </form>
             <p id="new_poll_title">{this.state.post_text}</p>
             <p id="new_poll_category">Category: {this.state.poll_category}</p>
             <form onSubmit={this.submitHandler}>
